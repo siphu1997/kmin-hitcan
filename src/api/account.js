95ce511183928d1507be700ef613fb0c15b00fc7
Hồ Sĩ Phú <siphu1997@gmail.com>
2021-02-28 09:37:45 +0700

    clear

--- src/api/account.js
+++ src/api/account.js
@@ -1,24 +1,10 @@
 import AxiosService from 'configs/axiosService';
-function fakeLogin() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve({
-        data: {
-          name: 'A',
-        },
-        status: true,
-      });
-    }, 1500);
-  });
-}
+
 const AccountService = {
   getInfo() {
     return AxiosService.get('/info').then((res) => {
       return res.data || { status: false };
     });
-  },
-  getInfoFake() {
-    return fakeLogin().then((res) => res.data || { status: false });
   },
 };
 
